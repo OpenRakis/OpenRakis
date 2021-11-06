4b19af7359a8a273f36d177dd2afcde0ab3a9290
Maximilien Noal <noal.maximilien@gmail.com>
2021-11-06 11:38:22 +0100

    DuneExtractor: fixed directories not being written
    Signed-off-by: Maximilien Noal <noal.maximilien@gmail.com>

--- tools/cd/DuneExtractor/DuneExtractor.cs
+++ tools/cd/DuneExtractor/DuneExtractor.cs
@@ -5,10 +5,9 @@
     using System.IO;
     using System.Text;
 
-    using Microsoft.VisualBasic;
     using Microsoft.VisualBasic.CompilerServices;
 
-    internal static class DuneExtractor
+    internal class DuneExtractor
     {
         internal static void Print_Welcome()
         {
@@ -23,22 +22,6 @@
             Console.WriteLine("It will creates a folder with all files extracted.");
         }
 
-        public static void WriteLog(string log, string folder)
-        {
-            var contents = $"Unofficial Dune (PC VERSION) Extractor V1.0{Environment.NewLine}Tgames (c) 2019{Environment.NewLine}{Environment.NewLine}-------------- Begin Log File --------------{Environment.NewLine}{Environment.NewLine}{log}{Environment.NewLine}{Environment.NewLine}-------------- End Log File ----------------";
-            File.WriteAllText(Path.Combine(folder, $"{nameof(DuneExtractor)}.log"), contents, Encoding.Default);
-        }
-
-        public static string TimeToUnix(DateTime dteDate)
-        {
-            if (dteDate.IsDaylightSavingTime())
-            {
-                dteDate = DateAndTime.DateAdd(DateInterval.Hour, -1.0, dteDate);
-            }
-
-            return Conversions.ToString(DateAndTime.DateDiff(DateInterval.Second, new DateTime(621355968000000000L), dteDate));
-        }
-
         private static string UnicodeBytesToString(byte[] bytes) => Encoding.UTF8.GetString(bytes);
 
         private static int UnicodeBytesToInteger(byte[] bytes) => BitConverter.ToInt32(bytes, 0);
@@ -46,11 +29,11 @@
         public static byte[] ReadData(FileStream DatFile, int size)
         {
             int index = 0;
-            byte[] numArray = new byte[checked(size + 1)];
+            byte[] numArray = new byte[size + 1];
             while (index < DatFile.Length & index < size)
             {
-                numArray[index] = checked((byte)DatFile.ReadByte());
-                checked { ++index; }
+                numArray[index] = (byte)DatFile.ReadByte();
+                ++index;
             }
             return numArray;
         }
@@ -59,16 +42,16 @@
         {
             int index = 0;
             int newSize = 0;
-            byte[] array = new byte[checked(size + 1)];
+            byte[] array = new byte[size + 1];
             while (index < DatFile.Length & index < size)
             {
-                byte num = checked((byte)DatFile.ReadByte());
+                byte num = (byte)DatFile.ReadByte();
                 if (num > 0)
                 {
                     array[index] = num;
-                    checked { ++newSize; }
+                    ++newSize;
                 }
-                checked { ++index; }
+                ++index;
             }
             Array.Resize(ref array, newSize);
             return array;
@@ -81,61 +64,41 @@
             section.OffsetOfFile = UnicodeBytesToInteger(DuneExtractor.ReadData(DatFile, 4));
         }
 
-        public static void ReadWritetoFileStream(FileStream input, string target, int length)
+        private static void WriteDataSectionToDisk(Span<byte> dataInMemory, string outFolder, string outFile, int offset, int size)
         {
-            byte[] numArray = new byte[8193];
-            Stream stream = (Stream)File.OpenWrite(target);
-            int count = 1;
-            while (length > 0 & count > 0)
+            if (!Directory.Exists(outFolder))
             {
-                count = input.Read(numArray, 0, Math.Min(length, numArray.Length));
-                stream.Write(numArray, 0, count);
-                checked { length -= count; }
+                Directory.CreateDirectory(outFolder);
             }
-            stream.Close();
+            if (File.Exists(outFile))
+            {
+                File.Delete(outFile);
+            }
+
+            string targetPath = Path.Combine(outFolder, outFile);
+            if(!Directory.Exists(Path.GetDirectoryName(targetPath)))
+            {
+                Directory.CreateDirectory(Path.GetDirectoryName(targetPath));
+            }
+            File.WriteAllBytes(targetPath, dataInMemory.Slice(offset, size).ToArray());
+            Console.WriteLine($"Extracted to {targetPath}");
+            Console.WriteLine("");
         }
 
-        public static void ReadWriteSectionData(FileStream DatFile, string filepath, int offset, int size)
+        public static void ExtractDuneDatFile(string inputFile)
         {
-            string[] strArray = filepath.Split(Path.DirectorySeparatorChar);
-            string str = strArray[checked(strArray.Length - 1)];
-            string path = $"{filepath.Substring(0, filepath.LastIndexOf(Path.DirectorySeparatorChar))}{Path.DirectorySeparatorChar}";
-            DatFile.Seek(offset, SeekOrigin.Begin);
-            if (!Directory.Exists(path))
-            {
-                Directory.CreateDirectory(path);
-            }
-
-            if (File.Exists($"{path}{str}"))
-            {
-                File.Delete($"{path}{str}");
-            }
-
-            ReadWritetoFileStream(DatFile, path + str, size);
-        }
-
-        public static void ExtractDuneDatFile(string file)
-        {
-            string str1 = "";
-            string[] strArray = file.Split(Path.DirectorySeparatorChar);
-            string str2 = strArray[checked(strArray.Length - 1)];
-            string folder = file.Substring(0, file.LastIndexOf(Path.DirectorySeparatorChar)) + Path.DirectorySeparatorChar;
             Console.WriteLine($"[STARTING] Extraction of the DAT file has started...{Environment.NewLine}");
-            string str3 = str1 + $"[STARTING] Extraction of the DAT file has started...{Environment.NewLine}{Environment.NewLine}";
-            FileStream datafile = new FileStream(file, FileMode.Open);
+            var dataInMemory = File.ReadAllBytes(inputFile).AsSpan();
+            using FileStream datafile = new FileStream(inputFile, FileMode.Open);
             datafile.Seek(0L, SeekOrigin.Begin);
             Console.WriteLine($"[STEP 1] Reading DatFile header... PLEASE WAIT{Environment.NewLine}");
-            string str4 = str3 + $"[STEP 1] Reading DatFile header... PLEASE WAIT{Environment.NewLine}{Environment.NewLine}";
             byte[] numArray2 = new byte[2] { 61, 10 };
             byte[] numArray3 = ReadData(datafile, 2);
-            string log;
             if (numArray3[0] == numArray2[0] & numArray3[1] == numArray2[1])
             {
                 Console.WriteLine($"Cryo DatFile Detected... {Environment.NewLine}");
-                string str5 = str4 + $"DatFile Detected... {Environment.NewLine}{Environment.NewLine}";
                 Console.WriteLine($"[STEP 2] Extracting each files... PLEASE WAIT{Environment.NewLine}");
-                string str6 = str5 + $"[STEP 2] Extracting each files... PLEASE WAIT{Environment.NewLine}{Environment.NewLine}";
-                var arrayList = new List<DataSection>();
+                var dataList = new List<DataSection>();
                 int index = 0;
                 do
                 {
@@ -143,38 +106,27 @@
                     ReadDatFileSectionHeader(datafile, section);
                     if ((uint)Operators.CompareString(section.NameOfFile, "", false) > 0U)
                     {
-                        arrayList.Add(section);
+                        dataList.Add(section);
                         datafile.ReadByte();
                     }
                     else
                         break;
                 }
-                while (index < arrayList[0].OffsetOfFile);
-                folder = folder + str2 + $"_{Path.DirectorySeparatorChar}";
-                try
+                while (index < dataList[0].OffsetOfFile);
+                var outputFolder = $"{Path.GetFileName(inputFile)}_";
+                for (int i = 0; i < dataList.Count; i++)
                 {
-                    for (int i = 0; i < arrayList.Count; i++)
-                    {
-                        DataSection dataSection = arrayList[i];
-                        Console.WriteLine($"Extracting... {dataSection.NameOfFile} ({Math.Round(dataSection.SizeOfFile / 1024.0, 2)} KB)");
-                        str6 = $"{str6}Extracting... {dataSection.NameOfFile} ({Math.Round(dataSection.SizeOfFile / 1024.0, 2)} KB){Environment.NewLine}";
-                        ReadWriteSectionData(datafile, $"{folder}{dataSection.NameOfFile}", dataSection.OffsetOfFile, dataSection.SizeOfFile);
-                    }
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine($"{e.GetBaseException().GetType()}{Environment.NewLine}{e.GetBaseException().Message}{Environment.NewLine}{e.GetBaseException().StackTrace}");
+                    DataSection dataSection = dataList[i];
+                    Console.WriteLine($"Extracting... {dataSection.NameOfFile} ({Math.Round(dataSection.SizeOfFile / 1024.0, 2)} KB)");
+                    WriteDataSectionToDisk(dataInMemory, outputFolder, dataSection.NameOfFile, dataSection.OffsetOfFile, dataSection.SizeOfFile);
                 }
                 Console.WriteLine($"{Environment.NewLine}[DONE] Extraction finished !{Environment.NewLine}");
-                log = $"{str6}{Environment.NewLine}[DONE] Extraction finished !{Environment.NewLine}";
             }
             else
             {
-                Console.WriteLine($"FATAL ERROR: {file} is not a valid Cryo DatFile !{Environment.NewLine}");
-                log = $"{str4}FATAL ERROR: {file} is not a valid Cryo DatFile !{Environment.NewLine}{Environment.NewLine}";
+                Console.WriteLine($"FATAL ERROR: {inputFile} is not a valid Cryo DatFile !{Environment.NewLine}");
             }
             datafile.Close();
-            WriteLog(log, folder);
         }
     }
 }
