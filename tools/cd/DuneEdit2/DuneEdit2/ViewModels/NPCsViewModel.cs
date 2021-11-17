da06660f58239368148716e63131671798d92107
Maximilien Noal <noal.maximilien@gmail.com>
2021-11-17 23:03:32 +0100

    DuneEdit2: began NPCs, Smugglers tabs (ODRADE)
    Signed-off-by: Maximilien Noal <noal.maximilien@gmail.com>

---
+++ tools/cd/DuneEdit2/DuneEdit2/ViewModels/NPCsViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace DuneEdit2.ViewModels
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    internal class NPCsViewModel
+    {
+    }
+}
