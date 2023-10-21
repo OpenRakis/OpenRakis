79a22af323f4902a87d97c52d6fb76eea32c76ab
Maximilien Noal <noal.maximilien@gmail.com>
2023-10-21 10:18:15 +0200

    Save uncompressed save to file
    Signed-off-by: Maximilien Noal <noal.maximilien@gmail.com>

--- tools/cd/DuneSaveDescriptor/Program.cs
+++ tools/cd/DuneSaveDescriptor/Program.cs
@@ -1,3 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using DuneSaveDescriptor.Decompression;
+
+if (!args.Any())
+{
+    Console.WriteLine("Please specify a path to a DUNE 3.7 sav file as argument");
+}
+var saveFile = args[0];
+if (!File.Exists(saveFile))
+{
+    Console.WriteLine("File does not exit or is unreachable");
+}
+
+var compressedSaveFile = File.ReadAllBytes(saveFile);
+var uncompressedData = Decompressor.Decompress(compressedSaveFile);
+File.WriteAllBytes($"{saveFile}.BIN", uncompressedData);
