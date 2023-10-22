537cbf42cddddcbecf9b7fc9c23e5c11b5bf97b6
Maximilien Noal <noal.maximilien@gmail.com>
2023-10-22 12:18:29 +0200

    Removed checked arithmetic (decompiler noise)
    Signed-off-by: Maximilien Noal <noal.maximilien@gmail.com>

--- tools/cd/DuneSaveDescriptor/Decompression/Decompressor.cs
+++ tools/cd/DuneSaveDescriptor/Decompression/Decompressor.cs
@@ -35,110 +35,102 @@ internal static class Decompressor
     public static byte[] Decompress(byte[] compressedData)
     {
         var uncompressedData = new List<byte>();
-        checked
+        int overallLength = compressedData.Length - 0x3;
+        var traps = DetectTraps(compressedData);
+        var controls = new List<Control>();
+        int length = overallLength;
+        int offset = 0;
+        while (true)
         {
-            int overallLength = compressedData.Length - 0x3;
-            var traps = DetectTraps(compressedData);
-            var controls = new List<Control>();
-            int length = overallLength;
-            int offset = 0;
-            while (true)
+            int innerLength = length;
+            if (offset > innerLength)
             {
-                int innerLength = length;
-                if (offset > innerLength)
+                break;
+            }
+            byte firstByte = compressedData[offset];
+            byte secondByte = compressedData[offset + 0x1];
+            byte thirdByte = compressedData[offset + 0x2];
+            byte[] byteArray = new byte[0x3] { firstByte, secondByte, thirdByte };
+            if (SequenceParser.IsControlSequence(byteArray))
+            {
+                Control control = new(new byte[0x3] { firstByte, secondByte, thirdByte }, uncompressedData.Count);
+                controls.Add(control);
+                uncompressedData.Add(0xF7);
+                offset += 0x2;
+            }
+            else
+            {
+                bool isTrap = IsTrap(traps, offset);
+                if (SequenceParser.IsDeflateSequence(byteArray))
                 {
-                    break;
-                }
-                byte firstByte = compressedData[offset];
-                byte secondByte = compressedData[offset + 0x1];
-                byte thirdByte = compressedData[offset + 0x2];
-                byte[] byteArray = new byte[0x3] { firstByte, secondByte, thirdByte };
-                if (SequenceParser.IsControlSequence(byteArray))
-                {
-                    Control control = new(new byte[0x3] { firstByte, secondByte, thirdByte }, uncompressedData.Count);
-                    controls.Add(control);
-                    uncompressedData.Add(0xF7);
+                    if (isTrap)
+                    {
+                        SetRealOffset(traps, offset, uncompressedData.Count);
+                    }
+
+                    int count = 0x1;
+                    while (true)
+                    {
+                        innerLength = secondByte;
+                        if (count <= innerLength)
+                        {
+                            uncompressedData.Add(thirdByte);
+                            count++;
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+
                     offset += 0x2;
                 }
                 else
                 {
-                    bool isTrap = IsTrap(traps, offset);
-                    if (SequenceParser.IsDeflateSequence(byteArray))
+                    uncompressedData.Add(firstByte);
+                    if (offset == overallLength)
                     {
-                        if (isTrap)
-                        {
-                            SetRealOffset(traps, offset, uncompressedData.Count);
-                        }
-
-                        int count = 0x1;
-                        while (true)
-                        {
-                            innerLength = secondByte;
-                            if (count <= innerLength)
-                            {
-                                uncompressedData.Add(thirdByte);
-                                count++;
-                            }
-                            else
-                            {
-                                break;
-                            }
-                        }
-
-                        offset += 0x2;
-                    }
-                    else
-                    {
-                        uncompressedData.Add(firstByte);
-                        if (offset == overallLength)
-                        {
-                            uncompressedData.Add(secondByte);
-                            uncompressedData.Add(thirdByte);
-                        }
+                        uncompressedData.Add(secondByte);
+                        uncompressedData.Add(thirdByte);
                     }
                 }
-
-                offset++;
             }
-            return uncompressedData.ToArray();
+
+            offset++;
         }
+        return uncompressedData.ToArray();
     }
     
     private static bool IsTrap(IReadOnlyList<Trap> traps, int index)
     {
-        bool result = false;
-        for (int i = 0x0; i < traps.Count; i++)
+        for (int i = 0; i < traps.Count; i++)
         {
             Trap trap = traps[i];
             if (trap.Offset == index)
             {
-                result = true;
-                break;
+                return true;
             }
         }
-        return result;
+        return false;
     }
     
     private static void SetRealOffset(IReadOnlyList<Trap> traps, int index, int value)
     {
-        checked
+        int length = traps.Count - 1;
+        int count = 0;
+        while (true)
         {
-            int length = traps.Count - 0x1;
-            int count = 0x0;
-            while (true)
+            if (count <= length)
             {
-                if (count <= length)
+                if (traps[count].Offset == index)
                 {
-                    if (traps[count].Offset == index)
-                    {
-                        traps[count].RealOffset = value;
-                        break;
-                    }
-                    count++;
-                    continue;
+                    traps[count].RealOffset = value;
+                    break;
                 }
-                break;
+                count++;
+                continue;
             }
+            break;
         }
     }
 }
