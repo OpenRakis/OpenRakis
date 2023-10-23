f68727245acd9540041767c093e8ac97ebbbfa40
Maximilien Noal <noal.maximilien@gmail.com>
2023-10-23 23:13:46 +0200

    CSV: Strucutre Name Column
    Signed-off-by: Maximilien Noal <noal.maximilien@gmail.com>

--- tools/cd/DuneEdit2/DuneEdit2/Models/SaveGameOffsets.cs
+++ tools/cd/DuneEdit2/DuneEdit2/Models/SaveGameOffsets.cs
@@ -1,4 +1,6 @@
-﻿namespace DuneEdit2.Models;
+﻿using System.Collections;
+
+namespace DuneEdit2.Models;
 
 public  interface ISaveGameOffsets
 {
@@ -19,68 +21,68 @@ public  interface ISaveGameOffsets
 
 public record Dune21Offsets : ISaveGameOffsets
 {
-    int ISaveGameOffsets.TimeCounters { get { return 17339;}}
-    int ISaveGameOffsets.Troops { get { return 19557;}}
-    int ISaveGameOffsets.Locations { get { return  17695;}}
-    int ISaveGameOffsets.NPCs { get { return  21393;}}
-    int ISaveGameOffsets.Smugglers { get { return  21393;}}
-    int ISaveGameOffsets.Charisma { get { return  17480;}} // TBD
-    int ISaveGameOffsets.ContactDistance { get { return  21909;}}// TBD
-    int ISaveGameOffsets.DateTime { get { return  21907;}}// TBD
-    int ISaveGameOffsets.DateTimeSaveScreen { get { return  0;}}// TBD
-    int ISaveGameOffsets.GameStage { get { return  17481;}}// TBD
+    int ISaveGameOffsets.TimeCounters => 17339;
+    int ISaveGameOffsets.Troops => 19557;
+    int ISaveGameOffsets.Locations => 17695;
+    int ISaveGameOffsets.NPCs => 21393;
+    int ISaveGameOffsets.Smugglers => 21393;
+    int ISaveGameOffsets.Charisma => 17480; // TBD
+    int ISaveGameOffsets.ContactDistance => 21909; // TBD
+    int ISaveGameOffsets.DateTime => 21907; // TBD
+    int ISaveGameOffsets.DateTimeSaveScreen => 0; // TBD
+    int ISaveGameOffsets.GameStage => 17481; // TBD
     int ISaveGameOffsets.NumberOfRalliedTroops => 17479; //TBD
-    int ISaveGameOffsets.Spice { get { return  17599;}} // TBD
-    int ISaveGameOffsets.Dialogues { get { return 13113;}} // TBD
+    int ISaveGameOffsets.Spice => 17599; // TBD
+    int ISaveGameOffsets.Dialogues => 13113; // TBD
 }
 
 public record Dune23Offsets : ISaveGameOffsets
 {
-    int ISaveGameOffsets.TimeCounters { get { return  17359;}}
-    int ISaveGameOffsets.Troops { get { return  19577;}}
-    int ISaveGameOffsets.Locations { get { return  17615;}}
-    int ISaveGameOffsets.NPCs { get { return  21413;}}
-    int ISaveGameOffsets.Smugglers { get { return  21671;}}
-    int ISaveGameOffsets.Charisma { get { return  17480;}} // TBD
-    int ISaveGameOffsets.ContactDistance { get { return  21909;}} // TBD
-    int ISaveGameOffsets.DateTime { get { return  21907;}} // TBD
-    int ISaveGameOffsets.DateTimeSaveScreen { get { return  0;}} // TBD
+    int ISaveGameOffsets.TimeCounters => 17359;
+    int ISaveGameOffsets.Troops => 19577;
+    int ISaveGameOffsets.Locations => 17615;
+    int ISaveGameOffsets.NPCs => 21413;
+    int ISaveGameOffsets.Smugglers => 21671;
+    int ISaveGameOffsets.Charisma => 17480; // TBD
+    int ISaveGameOffsets.ContactDistance => 21909; // TBD
+    int ISaveGameOffsets.DateTime => 21907; // TBD
+    int ISaveGameOffsets.DateTimeSaveScreen => 0; // TBD
     int ISaveGameOffsets.NumberOfRalliedTroops => 17479; //TBD
-    int ISaveGameOffsets.GameStage { get { return  17481;}} // TBD
-    int ISaveGameOffsets.Spice { get { return  17599;}} // TBD
-    int ISaveGameOffsets.Dialogues { get { return  13113;}} // TBD
+    int ISaveGameOffsets.GameStage => 17481; // TBD
+    int ISaveGameOffsets.Spice => 17599; // TBD
+    int ISaveGameOffsets.Dialogues => 13113; // TBD
 }
 
 public record Dune24Offsets : ISaveGameOffsets
 {
-    int ISaveGameOffsets.TimeCounters { get { return  17369;}}
-    int ISaveGameOffsets.Troops { get { return  19587;}}
-    int ISaveGameOffsets.Locations { get { return  17625;}}
-    int ISaveGameOffsets.NPCs { get { return  21423;}}
-    int ISaveGameOffsets.Smugglers { get { return  21681;}}
-    int ISaveGameOffsets.Charisma { get { return  17480;}} // TBD
-    int ISaveGameOffsets.ContactDistance { get { return  21909;}} // TBD
-    int ISaveGameOffsets.DateTime { get { return  21907;}} // TBD
-    int ISaveGameOffsets.DateTimeSaveScreen { get { return  0;}} // TBD
+    int ISaveGameOffsets.TimeCounters => 17369;
+    int ISaveGameOffsets.Troops => 19587;
+    int ISaveGameOffsets.Locations => 17625;
+    int ISaveGameOffsets.NPCs => 21423;
+    int ISaveGameOffsets.Smugglers => 21681;
+    int ISaveGameOffsets.Charisma => 17480; // TBD
+    int ISaveGameOffsets.ContactDistance => 21909; // TBD
+    int ISaveGameOffsets.DateTime => 21907; // TBD
+    int ISaveGameOffsets.DateTimeSaveScreen => 0; // TBD
     int ISaveGameOffsets.NumberOfRalliedTroops => 17479; //TBD
-    int ISaveGameOffsets.GameStage { get { return  17481;}} // TBD
-    int ISaveGameOffsets.Spice { get { return  17599;}} // TBD
-    int ISaveGameOffsets.Dialogues { get { return  13113;}} // TBD
+    int ISaveGameOffsets.GameStage => 17481; // TBD
+    int ISaveGameOffsets.Spice => 17599; // TBD
+    int ISaveGameOffsets.Dialogues => 13113; // TBD
 }
 
 public record Dune37Offsets : ISaveGameOffsets
 {
-    int ISaveGameOffsets.TimeCounters { get { return  17439;}}
-    int ISaveGameOffsets.Troops { get { return  19657;}}
-    int ISaveGameOffsets.Locations { get { return  17695;}}
-    int ISaveGameOffsets.NPCs { get { return  21493;}}
-    int ISaveGameOffsets.Smugglers { get { return  21751;}}
-    int ISaveGameOffsets.Charisma { get { return  17480;}}
-    int ISaveGameOffsets.ContactDistance { get { return  21909;}}
-    int ISaveGameOffsets.DateTime { get { return  21907;}}
-    int ISaveGameOffsets.DateTimeSaveScreen { get { return  0;}}
+    int ISaveGameOffsets.TimeCounters => 17439;
+    int ISaveGameOffsets.Troops => 19657;
+    int ISaveGameOffsets.Locations => 17695;
+    int ISaveGameOffsets.NPCs => 21493;
+    int ISaveGameOffsets.Smugglers => 21751;
+    int ISaveGameOffsets.Charisma => 17480;
+    int ISaveGameOffsets.ContactDistance => 21909;
+    int ISaveGameOffsets.DateTime => 21907;
+    int ISaveGameOffsets.DateTimeSaveScreen => 0;
     int ISaveGameOffsets.NumberOfRalliedTroops => 17479;
-    int ISaveGameOffsets.GameStage { get { return  17481;}}
-    int ISaveGameOffsets.Spice { get { return  17599;}}
-    int ISaveGameOffsets.Dialogues { get { return  13113;}}
+    int ISaveGameOffsets.GameStage => 17481;
+    int ISaveGameOffsets.Spice => 17599;
+    int ISaveGameOffsets.Dialogues => 13113;
 }
--- tools/cd/DuneSaveDescriptor/CSV/SaveFileCsv.cs
+++ tools/cd/DuneSaveDescriptor/CSV/SaveFileCsv.cs
@@ -1,53 +1,22 @@
 using System.Text;
+using DuneEdit2.Models;
 
 namespace DuneSaveDescriptor.CSV;
 
 using static System.FormattableString;
 internal static class SaveFileCsv
 {
-    private const string Headers = "Linear Address,Bytes (16), As ASCII";
-    private const int BytesColumnLength = 16;
-
-    private const char CsvSeparator = ',';
+    private const string Headers = "Structure Name, Address, Length, Bytes, Value, Notes";
+    private static readonly ISaveGameOffsets _dune37SaveGameOffsets = new Dune37Offsets();
+    private const char Comma = ',';
 
     public static IEnumerable<string> GenerateLines(DecompressedSave uncompressedSave)
     {
         yield return Headers;
-        // TOOD: Display each new high level data start bytes on a new line, so the struct name sequence and other columns cells
-        // only contain one value on each line.
-        // Else, the CSV will not be readable.
-        for (int i = 0; i < uncompressedSave.DecompressedData.Length; i += BytesColumnLength)
+        //TODO: Generate a Dictionary where the key is the structure name
+        foreach (var s in new (string name, int address)[] { (nameof(_dune37SaveGameOffsets.GameStage), _dune37SaveGameOffsets.GameStage) })
         {
-            byte[] dataPart = uncompressedSave.DecompressedData[i..];
-            yield return Invariant($"{i}{CsvSeparator}{GetBytesCell(dataPart)}{CsvSeparator}{GetAsciiCell(dataPart)}");
+            yield return Invariant($"{s.name}{Comma}{s.address}");
         }
-    }
-    
-    private static string GetBytesCell(byte[] uncompressedData)
-    {
-        return Convert.ToHexString(uncompressedData[..Math.Min(uncompressedData.Length, BytesColumnLength)]);
-    }
-    
-    /// <summary>
-    /// TODO: Remove this column, and display high level data instead
-    /// </summary>
-    /// <param name="uncompressedData"></param>
-    /// <returns></returns>
-    private static string GetAsciiCell(byte[] uncompressedData)
-    {
-        var str = Encoding.ASCII.GetString(uncompressedData[..Math.Min(uncompressedData.Length, BytesColumnLength)]);
-        var displayableStr = new StringBuilder();
-        foreach(var character in str)
-        {
-            if(char.IsControl(character))
-            {
-                displayableStr.Append('.');
-            }
-            else
-            {
-                displayableStr.Append(character);
-            }
-        }
-        return displayableStr.ToString();
     }
 }
